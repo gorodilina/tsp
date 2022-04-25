d6852df91e76fd27c777593eb0453c43fc7c3586
gorodilina <104147754+gorodilina@users.noreply.github.com>
2022-04-25 15:46:39 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,4 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-void func ();
+
+struct pt
+{
+    //int id;
+    double x,y;
+};
+
+double dist( pt a, pt b );
+
+double min_elem (vector<double> &v);
+
+double max_elem (vector<double> &v);
+
+vector<vector<double>> min_row (vector<vector<double>> &M);
+
+vector<vector<double>> coeffs (vector<vector<double>> &M);
+
+void show_mat(vector<vector<double>> &M);
+
+pt max_coeff (vector<vector<double>> &C);
+
+vector<vector<double>> get_M1 (vector<vector<double>> &M, vector<vector<double>> &C);
+
+vector<vector<double>> get_M2 (vector<vector<double>> &M, vector<vector<double>> &C);
+
+void little_method(vector<vector<double>> &M);
 
